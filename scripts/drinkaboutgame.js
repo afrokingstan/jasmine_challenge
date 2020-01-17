a7555cbdeb3ec07515e33a3fc8ddd3345b548fa7
Ubuntu <ubuntu@ip-172-31-94-46.ec2.internal>
2020-01-17 16:35:57 +0000

    first commit

---
+++ scripts/drinkaboutgame.js
@@ -0,0 +1,3 @@
+function drinkingage() {
+return -1;
+}
---
+++ spec/drinkaboutgameSpec.js
@@ -0,0 +1,7 @@
+describe("drinkaboutgame", function() {
+    describe("Drinkingage test", function() {
+        it("should return -1", function() {
+            expect(drinkingage(0, 1)).toBe(-1)
+        });
+    });
+});
