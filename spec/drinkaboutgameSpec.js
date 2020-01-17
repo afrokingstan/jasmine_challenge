ae625327347b9c77b74ff1ea7ae2bcef40e16d20
Ubuntu <ubuntu@ip-172-31-94-46.ec2.internal>
2020-01-17 16:44:18 +0000

    second commit

--- spec/drinkaboutgameSpec.js
+++ spec/drinkaboutgameSpec.js
@@ -3,5 +3,9 @@ describe("drinkaboutgame", function() {
         it("should return -1", function() {
             expect(drinkingage(0, 1)).toBe(-1)
         });
+        it("should return 13", function() {
+            expect(drinkingage(14, 1).toBe(13))
+        });
+
     });
 });
