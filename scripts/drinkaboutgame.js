3b19386c6112c4e0071810781285b9de755ad78d
Ubuntu <ubuntu@ip-172-31-94-46.ec2.internal>
2020-01-20 13:55:50 +0000

    5th commit

--- scripts/drinkaboutgame.js
+++ scripts/drinkaboutgame.js
@@ -1,7 +1,20 @@
-function drinkingage(actualAge) {
-    if(typeof(actualAge) == "number"){
-return actualAge  - 1;
-} else {
-return "Error!";
-}
-}
+whatCanIDrink = function(age) {
+	if (age < 0) {
+		return "Sorry. I can’t tell what drink because that age is incorrect!";
+	}
+	else if (age < 14) {
+		return "Drink Toddy";
+	}
+	else if (age < 18 && age >=14) {
+		return "Drink Coke";
+	}
+	else if (age < 21 && age >=18) {
+		return "Drink Beer";
+	}
+	else if (age < 130) {
+		return "Drink Whisky";
+	} 
+	else {
+		return "Sorry. I can’t tell what drink because that age is incorrect!"
+	}
+};
--- spec/drinkaboutgameSpec.js
+++ spec/drinkaboutgameSpec.js
@@ -1,19 +1,52 @@
-describe("drinkaboutgame", function() {
-    describe("Drinkingage test", function() {
-        it("should return -1", function() {
-            expect(drinkingage(0, 1)).toBe(-1)
+describe("My whatCanIDrink function", function() {
+    // A test suite begins with a call to the global Jasmine function describe with two parameters: a string and a function.
+    beforeEach(function() {
+        drink = new whatCanIDrink();
+    });
+    
+    describe("Checks age", function() {
+        // Specs are defined by calling the global Jasmine function it
+        it("should exist", function() {
+            // Expectations are built with the function expect which takes a value, called the actual.
+            // Each matcher implements a boolean comparison between the actual value and the expected value.
+            // Any matcher can evaluate to a negative assertion by chaining the call to expect with a not before calling the matcher.
+            expect(whatCanIDrink).toBeDefined();
         });
-        it("should return 13", function() {
-            expect(drinkingage(14, 1)).toBe(13)
+        
+        it("should return drink toddy when called as whatCanIDrink(13)", function() {
+            var result = whatCanIDrink(13)
+            expect(result).toBe("Drink Toddy");
         });
-        it("should return 17", function() {
-            expect(drinkingage(18, 1)).toBe(17)
+        
+        
+        it("should return drink coke when called as whatCanIDrink(17)", function() {
+            var result = whatCanIDrink(17)
+            expect(result).toBe("Drink Coke");
         });
-        it("should return 20", function() {
-            expect(drinkingage(21, 1)).toBe(20)
+        
+        it("should return drink beer when called as whatCanIDrink(18)", function() {
+            var result = whatCanIDrink(18)
+            expect(result).toBe("Drink Beer");
         });
-        it("should return 129", function() {
-            expect(drinkingage(130, 1)).toBe(129)
+        
+        it("should return drink beer when called as whatCanIDrink(20)", function() {
+            var result = whatCanIDrink(20)
+            expect(result).toBe("Drink Beer");
+        }); 
+        
+        it("should return drink whisky when called as whatCanIDrink(30)", function() {
+            var result = whatCanIDrink(30)
+            expect(result).toBe("Drink Whisky");
+        });
+        
+        it("should be unable to return a drink when called as whatCanIDrink(140)", function() {
+            var result = whatCanIDrink(140)
+            expect(result).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
+        });
+        
+        it("should contain Drink when called as whatCanIDrink(17)", function() {
+            var result = whatCanIDrink(10).indexOf("Drink");
+            expect(result).not.toEqual(-1);
         });
     });
 });
